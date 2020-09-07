d734eb340dc9aea8c028c102dc278dc6af4c3307
sb <sbinant@gmail.com>
2020-09-07 17:13:01 +0200

    Updates

---
+++ precache-manifest.a469bc5eb6fe79cf143d6480e58920b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0578873d52412b0d6c08",
+    "url": "/reactAppMemory/static/js/runtime~main.8841852e.js"
+  },
+  {
+    "revision": "fac4b88e5df759936946",
+    "url": "/reactAppMemory/static/js/main.8a92588c.chunk.js"
+  },
+  {
+    "revision": "e196f1d602d74cdb51ee",
+    "url": "/reactAppMemory/static/js/2.46dfbc90.chunk.js"
+  },
+  {
+    "revision": "fac4b88e5df759936946",
+    "url": "/reactAppMemory/static/css/main.79f9b522.chunk.css"
+  },
+  {
+    "revision": "1092563b1ab2ffb2f39e1217bab22803",
+    "url": "/reactAppMemory/index.html"
+  }
+];
